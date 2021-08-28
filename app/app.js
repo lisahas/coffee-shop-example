4838e1d047fb4a0853ee2db3b53256f97c62a69a
lisah <lisa.haskel@ndp-studio.com>
2021-08-28 12:01:16 +0100

    Skelaton files for dockerised express app WITH mysql and phpmyadmin images.
    Note we are using mysql2 node package to clear authentication issues with mysql8.
    
    Environment variables must be set in a text file called .env with contents eg:
    
    MYSQL_HOST=localhost
    MYSQL_USER=admin
    MYSQL_PASS=password
    MYSQL_ROOT_PASSWORD=foobar
    MYSQL_DATABASE=test
    MYSQL_ROOT_USER=root
    DB_CONTAINER=db
    DB_PORT=3306
    
    Phpmyadmin can be found at localhost:8081.
    Enter the db container with: docker exec -it <conttainer name> bash -l

--- app/app.js
+++ app/app.js
@@ -7,9 +7,53 @@ var app = express();
 // Add static files location
 app.use(express.static("static"));
 
+// Connect to the database
+// Import MySQL library
+const mysql = require('mysql2');
+
+// Get and check the environment variables (don't do this in production)
+require("dotenv").config();
+console.log(process.env);
+
+// Connect to the database.
+var con = mysql.createConnection({
+    host: process.env.DB_CONTAINER,
+    port: process.env.DB_PORT,
+    user: process.env.MYSQL_ROOT_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: "test"
+});
+
+// Function is callback when connection completed.
+// err is any error message that occurs
+con.connect(function(err) {
+    // If an error, print it out.
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Connected to test database.");
+});
+
+// Close the database connection.
+// Always close the connection when you are finished with it.
+// Function is callback when connection is closed.
+con.end(function(err) {
+    // If an error, print it out.
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Closed connection to test database.");
+});
+
 // Create a get for root - /
 app.get("/", function(req, res) {
     res.send("Hello world!");
+});
+
+// Create a route for testing the db
+app.get("/db_test", function(req, res) {
+  con.connect();
+  res.send("db tested!");
 });
 
 // Create a route for /goodbye
